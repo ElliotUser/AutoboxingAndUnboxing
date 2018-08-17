7c399264be62390f0f238f325cd3b54103b768c3
ElliotUser <andersen.e@bk.ru>
2018-08-17 21:40:36 +0300

    Оболочки типов, автоупаковка и автораспаковка

---
+++ src/exc02/AutoBox.java
@@ -0,0 +1,12 @@
+package exc02;
+
+public class AutoBox {
+    public static void main(String[] args) {
+
+        Integer iOb = 100;
+
+        int i = iOb;
+
+        System.out.println(i+" "+iOb);
+    }
+}
