600c93bfcd67f8d1c60cd323723af6b20002c9c6
ElliotUser <andersen.e@bk.ru>
2018-08-21 19:58:41 +0300

    add new exc

---
+++ src/exc05/AutoBox4.java
@@ -0,0 +1,11 @@
+package exc05;
+
+public class AutoBox4 {
+    public static void main(String[] args) {
+        Integer iOb = 100;
+        Double dOb = 98.9;
+
+        dOb = dOb + iOb;
+        System.out.println("dob после выражения: "+ dOb);
+    }
+}
---
+++ src/exc06/AutoBox5.java
@@ -0,0 +1,14 @@
+package exc06;
+
+public class AutoBox5 {
+    public static void main(String[] args) {
+        Boolean b = true;
+
+        if(b) System.out.println("b равно true");
+
+        Character ch = 'z';
+        char ch2 = ch;
+
+        System.out.println("ch2 равно "+ch2);
+    }
+}
